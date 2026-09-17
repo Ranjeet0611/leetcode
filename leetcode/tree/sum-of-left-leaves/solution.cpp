8a453942a7d37c966646c4b96ae7b08715d92db1
DevCache <devcache@users.noreply.github.com>
2026-09-17 17:43:27 +0530

    Add accepted solution for 404. Sum of Left Leaves

---
+++ leetcode/tree/sum-of-left-leaves/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            if(node->left!=NULL){
+                if(node->left->left==NULL && node->left->right==NULL){
+                    sum += node->left->val;
+                }
+                q.push(node->left);
+            }
+            if(node->right!=NULL){
+                q.push(node->right);
+            }
+        }
+        return sum;
+    }
+};
